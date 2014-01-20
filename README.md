b6bceb46559c9b73a2ce5a9c9b2654cd77e049a8
Dominic Watson <dominic@Dominics-iMac.local>
2014-01-20 17:27:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## AngularJS flicker directive
+
+Angular module for a flicker component that flicks/switches data.
+
+## Changelog
+
+**v0.0.1**
+
+    Initial commit
+
+## License
+
+Licensed under the MIT license
---
+++ app/scripts/controllers/flicker.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('angularFlicker')
+
+.controller('FlickerCtrl', function($scope) {
+
+    var users = [{
+        name: 'intellix',
+        amount: 243
+    }, {
+        name: 'and',
+        amount: 75
+    }, {
+        name: 'konoro',
+        amount: 122
+    }, {
+        name: 'kingdom',
+        amount: 252
+    }, {
+        name: 'are',
+        amount: 12
+    }, {
+        name: 'awesome',
+        amount: 22
+    }, {
+        name: 'really.',
+        amount: 223
+    }, {
+        name: 'honestly!',
+        amount: 52
+    }];
+
+    $scope.userChunks = users.chunk(3);
+
+});
